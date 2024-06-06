46d6c5100e42f59d974bbe2011a28b45998465e1
arindam923 <arindam92@proton.me>
2024-06-06 15:42:45 +0530

    change some services icons

--- src/components/home/ServiceSection.tsx
+++ src/components/home/ServiceSection.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const ServiceSection = () => {
   const cards = [
     {
@@ -5,23 +7,26 @@ const ServiceSection = () => {
       name: "Figma to React",
       description:
         "Transform your Figma designs into fully functional, high-performance React applications with pixel-perfect accuracy.",
-      image: " ",
+      image: "/arrow-48.png",
     },
     {
       id: 1,
       name: "Custom Web Development",
+      image: "/chat-48.png",
       description:
         " Tailor-made websites and web applications that perfectly align with your business goals and brand identity.",
     },
     {
       id: 2,
       name: "SEO Optimization",
+      image: "/location-48.png",
       description:
         "Enhance your online presence and drive organic traffic with our expert search engine optimization strategies.",
     },
     {
       id: 3,
       name: "Content Management Systems (CMS)",
+      image: "/trophy-48.png",
       description:
         " Implement and customize powerful CMS solutions like WordPress, Joomla, and Drupal to manage your content effortlessly.",
     },
@@ -45,7 +50,14 @@ const ServiceSection = () => {
             key={item.id}
             className="bg-neutral-900 flex flex-col justify-around border border-muted-foreground h-[200px] p-4 rounded-md"
           >
-            <div className="w-16 h-16 rounded-md bg-purple-100" />
+            {/* <div className="w-16 h-16 rounded-md bg-purple-100" /> */}
+            <Image
+              alt="service"
+              src={item.image}
+              width={30}
+              height={30}
+              className=""
+            />
             <h3 className="text-2xl font-semibold">{item.name}</h3>
             <p className="text-xs text-muted-foreground">{item.description}</p>
           </article>
